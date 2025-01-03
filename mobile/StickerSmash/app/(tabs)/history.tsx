aa7d26cbf5b5e7e525132a3dc327a00a96488f2d
EduardoHGomez <hurtado.josegomez@gmail.com>
2025-01-02 21:53:54 -0600

    history tsx

---
+++ mobile/StickerSmash/app/(tabs)/history.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { View, StyleSheet, Alert, Platform, Text } from 'react-native';
+import { Input, Button, Switch } from 'react-native-elements';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { supabase } from '../../lib/supabase';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function Index() {
+
+  useEffect(() => {
+  }, []);
+
+
+  return (
+    <View>
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
